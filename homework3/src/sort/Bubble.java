896d1deefafa976f8483960cbcc50b8414ec8e68
George Gu <george.zp.gu@gmail.com>
2017-05-02 20:53:42 -0400

    Additional whitespace fixes.

--- homework3/src/sort/Bubble.java
+++ homework3/src/sort/Bubble.java
@@ -2,49 +2,49 @@ package sort;
 
 public class Bubble {
 
-	public static int assignCount = 0;
+  public static int assignCount = 0;
 
-	public static int[] ascending(int[] intArray) {
-		int n = intArray.length;
-		int temp = 0;
-	
-		for(int i=0; i < n; i++){	
-			for(int j=1; j < (n-i); j++){
-				if(intArray[j-1] > intArray[j]){
-					temp = intArray[j]; //ASSIGNMENT
-					assignCount += 1;
-					intArray[j] = intArray[j - 1]; //ASSIGNMENT
-					assignCount += 1;
-					intArray[j - 1] = temp; //ASSIGNMENT
-					assignCount += 1;
-				}  
-			}
-		}
-	
-		//Print statements were removed, replaced with return statement
-		return intArray;
-	}
+  public static int[] ascending(int[] intArray) {
+    int n = intArray.length;
+    int temp = 0;
+  
+    for(int i=0; i < n; i++){  
+      for(int j=1; j < (n-i); j++){
+        if(intArray[j-1] > intArray[j]){
+          temp = intArray[j]; //ASSIGNMENT
+          assignCount += 1;
+          intArray[j] = intArray[j - 1]; //ASSIGNMENT
+          assignCount += 1;
+          intArray[j - 1] = temp; //ASSIGNMENT
+          assignCount += 1;
+        }  
+      }
+    }
+  
+    //Print statements were removed, replaced with return statement
+    return intArray;
+  }
 
-	public static int[] descending(int[] intArray) {
-		int n = intArray.length;
-		int temp = 0;
+  public static int[] descending(int[] intArray) {
+    int n = intArray.length;
+    int temp = 0;
 
-		for(int i=0; i < n; i++){
-			for(int j=1; j < (n-i); j++){
-				if(intArray[j-1] < intArray[j]){
-					temp = intArray[j-1]; //ASSIGNMENT
-					assignCount += 1;
-					intArray[j-1] = intArray[j]; //ASSIGNMENT
-					assignCount += 1;
-					intArray[j] = temp; //ASSIGNMENT
-					assignCount += 1;
-				} 
-			}
-		}
+    for(int i=0; i < n; i++){
+      for(int j=1; j < (n-i); j++){
+        if(intArray[j-1] < intArray[j]){
+          temp = intArray[j-1]; //ASSIGNMENT
+          assignCount += 1;
+          intArray[j-1] = intArray[j]; //ASSIGNMENT
+          assignCount += 1;
+          intArray[j] = temp; //ASSIGNMENT
+          assignCount += 1;
+        } 
+      }
+    }
 
-		//Print statements were removed, replaced with return statement
-		return intArray;
-	}
-	
+    //Print statements were removed, replaced with return statement
+    return intArray;
+  }
+  
 }
 
--- homework3/src/sort/Selection.java
+++ homework3/src/sort/Selection.java
@@ -1,53 +1,52 @@
 package sort; 
 
 public class Selection { 
-	
-	public static int assignCount = 0;
-	
+
+  public static int assignCount = 0;
+
   public static int[] ascending(int a[]) {
     int n = a.length;
-    
+
     for (int i = 0; i < n-1; i++) {
       int q = i;  
-      
+
       for (int j = i+1; j < n; j++) {
         if (a[j] < a[q]) {
           q = j; 
         }
       }
-      
-	    int temp = a[i]; //ASSIGNMENT
-	    assignCount += 1;
-	    a[i] = a[q]; //ASSIGNMENT
-	    assignCount += 1;
-	    a[q] = temp; //ASSIGNMENT
-	    assignCount += 1;
+
+      int temp = a[i]; //ASSIGNMENT
+      assignCount += 1;
+      a[i] = a[q]; //ASSIGNMENT
+      assignCount += 1;
+      a[q] = temp; //ASSIGNMENT
+      assignCount += 1;
     }
-    
+
     return a; 
   } 
-  
+
   public static int[] descending(int a[]) {
-	  int n = a.length;
-		
-		for (int i = 0; i < n-1; i++) {
-		  int q = i;
-		    
-		  for (int j = i+1; j < n; j++) {
-		    if (a[j] > a[q]) {
-		      q = j; 
-		    } 
-		  }
-		  
-			int temp = a[i]; //ASSIGNMENT
-			assignCount += 1;
-			a[i] = a[q]; //ASSIGNMENT
-			assignCount += 1;
-			a[q] = temp;  //ASSIGNMENT
-			assignCount += 1;
-	 }
-	 
-	  return a; 
+    int n = a.length;
+
+    for (int i = 0; i < n-1; i++) {
+      int q = i;
+
+      for (int j = i+1; j < n; j++) {
+        if (a[j] > a[q]) {
+          q = j; 
+        } 
+      }
+
+      int temp = a[i]; //ASSIGNMENT
+      assignCount += 1;
+      a[i] = a[q]; //ASSIGNMENT
+      assignCount += 1;
+      a[q] = temp;  //ASSIGNMENT
+      assignCount += 1;
+   }
+
+    return a; 
   } 
 }
-
